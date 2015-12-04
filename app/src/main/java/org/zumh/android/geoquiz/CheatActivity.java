d19ea493aa1805cc532143cba90d0f967bc7f397
tizz98 <dev.tizz98@gmail.com>
2015-12-04 13:48:12 -0500

    added cool button disappearing animation & show API level

--- app/src/main/java/org/zumh/android/geoquiz/CheatActivity.java
+++ app/src/main/java/org/zumh/android/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package org.zumh.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +22,7 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerShown = false;
 
     private TextView mAnswerTextView;
+    private TextView mAndroidVersionTextView;
     private Button mShowAnswer;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
@@ -43,7 +48,10 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+        mAndroidVersionTextView = (TextView) findViewById(R.id.android_version_text_view);
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+
+        mAndroidVersionTextView.setText("API level " + Build.VERSION.SDK_INT);
 
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +63,26 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 mAnswerShown = true;
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
